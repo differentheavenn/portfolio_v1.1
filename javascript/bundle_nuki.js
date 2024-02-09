1e2adb337e93b879bfe18c06d49a2caea574c87d
Different_Heaven <105215422+differentheavenn@users.noreply.github.com>
2024-02-09 20:46:21 +0700

    Update bundle_nuki.js

--- javascript/bundle_nuki.js
+++ javascript/bundle_nuki.js
@@ -1,4 +1,3 @@
-import { SpeedInsights } from "@vercel/speed-insights/next"
 const loader = document.querySelector("#loader_percentage");
 const elementsToLoad = document.querySelectorAll(
   "script, link[rel='stylesheet'], img, video, font"
